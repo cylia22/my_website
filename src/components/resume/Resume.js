b6d882f1c81564d9b5a0211396b5a1ed7961e751
zSyLL <77125297+cylia22@users.noreply.github.com>
2023-04-02 11:22:23 +0300

    Update Resume.js

--- src/components/resume/Resume.js
+++ src/components/resume/Resume.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import { Typography, Link } from '@material-ui/core';
-import { TextDecrypt } from '../content/TextDecrypt';
+//import { TextDecrypt } from '../content/TextDecrypt';
 //import ResumePDF from './../../assets/Cylia_Engels_Resume.pdf';
 import {
   ResumeIcon
